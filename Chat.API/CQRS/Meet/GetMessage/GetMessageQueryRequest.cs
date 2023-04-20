783160047549df97fe95cb7291ed8a5ee551bbcf
Burak Ali ILDIR <burakaliildir@gmail.com>
2023-04-20 18:57:14 +0300

    fix: page param default value

--- Chat.API/CQRS/Meet/GetMessage/GetMessageQueryRequest.cs
+++ Chat.API/CQRS/Meet/GetMessage/GetMessageQueryRequest.cs
@@ -8,7 +8,7 @@ namespace Chat.API.CQRS.Meet.GetMessage
     public class GetMessageQueryRequest : IRequest<GetMessageQueryResponse>
     {
         public string ReceiveId { get; set; }
-        public int Page { get; set; } = 0;
+        public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 10;
     }
 }
